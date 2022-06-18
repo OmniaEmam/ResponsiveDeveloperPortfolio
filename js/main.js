1c2225c2066356d5518ac0cc692bf1576bdc8bef
OmniaEmam <onieaemam@gmail.com>
2022-06-18 05:39:50 +0200

    Mobile screen

--- js/main.js
+++ js/main.js
@@ -99,7 +99,102 @@ modalCloses.forEach((modalClose) => {
 
 
 
+/*===================== PORTFOLIO SWIPER======================*/
+let swiperPortfolio = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
 
 
 
+/*===================== TESTIMONIAL SWIPER======================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
 
+    }
+});
+
+
+
+/*=====================SCROLL======================*/
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
+
+
+
+/*=====================Change Background======================*/
+function scrollHeader(){
+    const nav = document.getElementById("header");
+
+    if(this.scrollY >= 80){
+        nav.classList.add("scroll-header");
+    }
+    else{
+        nav.classList.remove("scroll-header");
+    }
+}
+
+window.addEventListener("scroll" , scrollHeader)
+
+
+/*=====================show scroll top======================*/
+function scrollUp(){
+    const scrollUp = document.getElementById("scroll-up");
+    if(this.scrollY >= 560){
+        scrollUp.classList.add("show-scroll");
+    }else{
+        scrollUp.classList.remove("show-scroll");
+    } 
+}
+
+window.addEventListener("scroll" , scrollUp);
+
+/*=====================DARK LIGH THEME======================*/
